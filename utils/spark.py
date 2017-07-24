0df8c770e33532978f923245a1f1ba07d3e5b552
zhexiao <zhexiao@163.com>
2017-07-24 09:53:50 +0000

    classerialzer

--- utils/spark.py
+++ utils/spark.py
@@ -3,24 +3,41 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
-sc = SparkContext(appName="spark_streaming")
-ssc = StreamingContext(sc, 5)
 
-zookeeper_host = '192.168.33.31:2181'
-group_ids = 'sparks'
-topics = {
-    'mnet': 1
-}
+class SparkStreaming:
+    spark_master = "spark://192.168.33.32:7077"
+    zookeeper_host = '192.168.33.31:2181'
+    group_id = 'spark-consumer'
+    topics = {
+        'mnet': 1
+    }
 
-kvs = KafkaUtils.createStream(ssc, zookeeper_host, group_ids, topics)
-data = kvs.map(lambda dt: dt[1])
+    def __init__(self):
+        # 创建streaming context
+        sc = SparkContext(master=self.spark_master, appName="spark_streaming")
+        ssc = StreamingContext(sc, 2)
 
-data.pprint()
+        # 链接到kafka zookeeper
+        dstream = KafkaUtils.createStream(
+            ssc, self.zookeeper_host, self.group_id, self.topics)
+        netflow_dstream = dstream.map(lambda ds: ds[1])
 
-# counts = lines.flatMap(lambda line: line.split(" ")) \
-#     .map(lambda word: (word, 1)) \
-#     .reduceByKey(lambda a, b: a + b)
-# counts.pprint()
+        # 打印数据
+        # netflow_dstream.pprint()
+        self.count_protocol(netflow_dstream)
 
-ssc.start()
-ssc.awaitTermination()
+        # 启动服务
+        ssc.start()
+        ssc.awaitTermination()
+
+    @staticmethod
+    def count_protocol(netflow_dstream):
+        """
+        给每个MAC地址统计protocol
+        :param netflow_dstream:
+        :return:
+        """
+
+
+# 运行类
+SparkStreaming()
