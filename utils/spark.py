5c808a67e8dbcac71c3b7ed08a0311f8d8dd6738
zhexiao <zhexiao@163.com>
2017-07-24 06:00:37 +0000

    add spark

---
+++ utils/spark.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+sc = SparkContext(appName="spark_streaming")
+ssc = StreamingContext(sc, 5)
+
+zookeeper_host = '192.168.33.31:2181'
+group_ids = 'sparks'
+topics = {
+    'mnet': 1
+}
+
+kvs = KafkaUtils.createStream(ssc, zookeeper_host, group_ids, topics)
+data = kvs.map(lambda dt: dt[1])
+
+data.pprint()
+
+# counts = lines.flatMap(lambda line: line.split(" ")) \
+#     .map(lambda word: (word, 1)) \
+#     .reduceByKey(lambda a, b: a + b)
+# counts.pprint()
+
+ssc.start()
+ssc.awaitTermination()
