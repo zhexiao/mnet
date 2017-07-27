d39393afc1c2cc6e07e87d9d90a2baefc3bafacb
zhexiao <zhexiao@163.com>
2017-07-27 08:53:46 +0000

    add dst ip and src ip

--- utils/spark.py
+++ utils/spark.py
@@ -24,9 +24,11 @@ class SparkStreaming:
     # 网络数据参数
     protocol_num = [6, 17]
     input_server_host = '192.168.33.39'
-    input_server_port = 21561
     udp_buffer_size = 1024
-    input_server_addr = (input_server_host, input_server_port)
+    input_server_src_ip_port = 21561
+    input_server_dst_ip_port = 21562
+    input_server_src_ip_addr = (input_server_host, input_server_src_ip_port)
+    input_server_dst_ip_addr = (input_server_host, input_server_dst_ip_port)
 
     def __init__(self):
         # 创建streaming context
@@ -42,11 +44,14 @@ class SparkStreaming:
         # netflow_dstream.pprint()
 
         # 统计数据
-        p_stats = self.count_protocol(netflow_dstream)
+        src_ip_stats, dst_ip_stats = self.count_protocol(netflow_dstream)
 
         # 对统计后的数据进行后续操作
-        p_stats.foreachRDD(
-            lambda rdd: self.process_result(rdd.collectAsMap())
+        src_ip_stats.foreachRDD(
+            lambda rdd: self.process_src_ip_result(rdd.collectAsMap())
+        )
+        dst_ip_stats.foreachRDD(
+            lambda rdd: self.process_dst_ip_result(rdd.collectAsMap())
         )
 
         # 启动服务
@@ -55,7 +60,7 @@ class SparkStreaming:
 
     def count_protocol(self, netflow_dstream):
         """
-        给每个MAC地址统计protocol
+        统计IP地址数据
         :param netflow_dstream:
         :return:
         """
@@ -65,27 +70,40 @@ class SparkStreaming:
         )
 
         # 重新map一个DStream，以源IP为key组装起来
-        p_mapped = p_flow.map(lambda dt: ((dt['netflow']['ipv4_src_addr']), (
-            1, dt['netflow']['in_pkts'], dt['netflow']['in_bytes']
-        )))
+        src_ip_mapped = p_flow.map(lambda dt: (
+            (dt['netflow']['ipv4_src_addr']),
+            (1, dt['netflow']['in_pkts'], dt['netflow']['in_bytes'])
+        ))
+        dst_ip_mapped = p_flow.map(lambda dt: (
+            (dt['netflow']['ipv4_dst_addr']),
+            (1, dt['netflow']['in_pkts'], dt['netflow']['in_bytes'])
+        ))
 
         # 先对数据做个小的统计
-        p_reduced = p_mapped.reduceByKey(lambda v1, v2: (
+        src_ip_reduced = src_ip_mapped.reduceByKey(lambda v1, v2: (
+            v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2]
+        ))
+        dst_ip_reduced = dst_ip_mapped.reduceByKey(lambda v1, v2: (
             v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2]
         ))
 
         # 然后对每30秒数据每10秒钟统计一次
-        p_stats = p_reduced.window(
+        src_ip_stats = src_ip_reduced.window(
+            self.window_duration, self.slide_duration
+        ).reduceByKey(
+            lambda v1, v2: (v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2])
+        )
+        dst_ip_stats = dst_ip_reduced.window(
             self.window_duration, self.slide_duration
         ).reduceByKey(
             lambda v1, v2: (v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2])
         )
 
-        return p_stats
+        return src_ip_stats, dst_ip_stats
 
-    def process_result(self, result):
+    def process_src_ip_result(self, result):
         """
-        处理统计得到的数据
+        处理统计得到的src ip数据
         :return:
         """
         for ip, val in result.iteritems():
@@ -95,26 +113,71 @@ class SparkStreaming:
                 'packets': val[1],
                 'bytes': val[2]
             }
-            pprint('+'*50)
+            pprint('src ip - '*50)
+            pprint('+' * 50)
+            pprint('+' * 50)
             pprint(json.dumps(json_data))
             pprint('+' * 50)
+            pprint('+' * 50)
+            pprint('src ip - ' * 50)
 
-            self.send_data(json.dumps(json_data))
+            self.send_src_ip_data(json.dumps(json_data))
 
-    def send_data(self, json_string):
+    def process_dst_ip_result(self, result):
         """
-        使用网络传输数据到input server的logstash
+        处理统计得到的dst ip数据
+        :return:
+        """
+        for ip, val in result.iteritems():
+            json_data = {
+                'ip': ip,
+                'flows': val[0],
+                'packets': val[1],
+                'bytes': val[2]
+            }
+            pprint('dst ip - '*50)
+            pprint('+' * 50)
+            pprint('+' * 50)
+            pprint(json.dumps(json_data))
+            pprint('+' * 50)
+            pprint('+' * 50)
+            pprint('dst ip -' * 50)
+
+            self.send_dst_ip_data(json.dumps(json_data))
+
+    def send_src_ip_data(self, json_string):
+        """
+        使用网络传输src ip数据到input server的logstash
         :param json_string:
         :return:
         """
         udp_socket = socket(AF_INET, SOCK_DGRAM)
         try:
-            udp_socket.sendto(json_string, self.input_server_addr)
+            udp_socket.sendto(json_string, self.input_server_src_ip_addr)
         except socket.error:
             pprint('+' * 50)
             pprint("cannot connect to {0}:{1}".format(
                 self.input_server_host,
-                self.input_server_port
+                self.input_server_src_ip_port
+            ))
+            pprint('+' * 50)
+        finally:
+            udp_socket.close()
+
+    def send_dst_ip_data(self, json_string):
+        """
+        使用网络传输dst ip数据到input server的logstash
+        :param json_string:
+        :return:
+        """
+        udp_socket = socket(AF_INET, SOCK_DGRAM)
+        try:
+            udp_socket.sendto(json_string, self.input_server_dst_ip_addr)
+        except socket.error:
+            pprint('+' * 50)
+            pprint("cannot connect to {0}:{1}".format(
+                self.input_server_host,
+                self.input_server_dst_ip_port
             ))
             pprint('+' * 50)
         finally:
