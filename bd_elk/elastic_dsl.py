9a089d984bf77b3f8556975609cd27a480214f72
zhexiao <zhexiao@163.com>
2017-07-31 05:43:11 +0000

    add es class

---
+++ bd_elk/elastic_dsl.py
@@ -0,0 +1,16 @@
+from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl import DocType, String, Long
+
+ELASTIC_HOST = "192.168.33.35"
+ELASTIC_PORT = 9200
+connections.create_connection(hosts=[ELASTIC_HOST])
+
+
+class Logs(DocType):
+    flows = Long()
+    bytes = Long()
+    packets = Long()
+    ip = String()
+
+    class Meta:
+        index = 'src-ip-stats-2017.07.27'
--- netflow/views.py
+++ netflow/views.py
@@ -1,7 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from bd_elk.elastic_dsl import Logs
 
 
 class TestApi(APIView):
     def get(self, request, format=None):
+        # s = ElasticDsl().search(index="src-ip-stats-2017.07.27")
+        # s.query("match", ip="36.110.147.35")
+        # response = s.execute()
+        # print(s)
+        # print(json.dumps(s.to_dict()))
+
+        dt = Logs.get(id="AV2EG7SSKstPrjF46rlT")
+        print(dt.to_dict())
+
+        s = Logs.search().query("match", ip="36.110.147.35")
+        response = s.scan()
+        print('Total %d hits found.' % s.count())
+        for h in response:
+            print(h)
         return Response("test")
