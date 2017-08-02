c17de35bcfa0a7a78129e0fb08e6e582eef90bd0
zhexiao <zhexiao@163.com>
2017-08-02 11:48:40 +0800

    spark shell

--- bd_spark/start_spark.sh
+++ bd_spark/start_spark.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "kill spark.py ......"
+pkill -f spark.py
+
 echo "stop spark master ......"
 /opt/spark/sbin/stop-master.sh
 sleep 3
@@ -14,9 +17,6 @@ sleep 3
 echo "start spark slaves ......"
 /opt/spark/sbin/start-slaves.sh
 sleep 3
-
-echo "kill spark.py ......"
-pkill -f spark.py
 
 echo "start spark.py ......"
 /opt/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1 /home/hdgs/mnet/bd_spark/spark.py
