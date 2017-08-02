40e5d79cdb589549240b4705328ee28a40323483
zhexiao <zhexiao@163.com>
2017-08-02 01:17:11 +0000

    add running shell

---
+++ bd_spark/run_spark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/opt/spark/sbin/stop-master.sh
+/opt/spark/sbin/stop-slaves.sh
+
+/opt/spark/sbin/start-master.sh
+/opt/spark/sbin/start-slaves.sh
+
+/opt/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1 /home/hdgs/mnet/bd_spark/spark.py
