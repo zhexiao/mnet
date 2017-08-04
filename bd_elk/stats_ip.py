c295fb7c5b57f0bfa067555479f306c0c24a35e9
Zhe Xiao <zhexiao@163.com>
2017-08-04 15:22:45 +0800

    merge dev (#10)
    * add ip factory class
    
    * add pandas and common cache
    
    * change es class
    
    * fix netflow
    
    * init constants
    
    * add constants

--- bd_elk/stats_ip.py
+++ bd_elk/stats_ip.py
@@ -1,14 +1,18 @@
 # -*- coding: UTF-8 -*-
 from elasticsearch_dsl import DocType, Keyword, Long
-from bd_elk.common_doc import CommonDoc
-from django.core.cache import cache
+
+from bd_elk.common_es import CommonEs
+from ultis.commons import ComFunc
 
 
-class CommonIp(DocType):
+class CommonIp(DocType, CommonEs):
     # todo should use pandas to do json format
     """
-    Common Ip Func
+    基于Ip 数据公用的类
     """
+
+    # 指定的类型（src，dst...）
+    _type = None
 
     @classmethod
     def get_stats(cls, **kwargs):
@@ -16,10 +20,8 @@ class CommonIp(DocType):
         get ip stats
         :return:
         """
-        type = kwargs.get('type')
-
-        cache_key = 'ip-stats-{0}'.format(type)
-        json_res = cache.get(cache_key)
+        cache_key = 'ip-stats-{0}'.format(cls._type)
+        json_res = ComFunc.cache(cache_key)
 
         if not json_res:
             s = cls.search().extra(size=0)
@@ -31,17 +33,22 @@ class CommonIp(DocType):
             # cls.debug_query(s)
             response = s.execute()
 
-            json_res = {'ip': [], 'flows': [], 'bytes': [], 'packets': []}
+            json_res = []
             for stats in response.aggregations.ip_terms.buckets:
-                json_res['ip'].append(stats.key)
-                json_res['flows'].append(stats.flows_per_ip.value)
-                json_res['bytes'].append(
-                    cls.bytes_convert(stats.bytes_per_ip.value, 'kb')
-                )
-                json_res['packets'].append(stats.packets_per_ip.value)
+                json_res.append({
+                    'ip': stats.key,
+                    'flows': ComFunc.number_convert(
+                        stats.flows_per_ip.value, 'k'
+                    ),
+                    'packets': ComFunc.number_convert(
+                        stats.packets_per_ip.value, 'k'
+                    ),
+                    'bytes': ComFunc.bytes_convert(
+                        stats.bytes_per_ip.value, 'mb'
+                    )
+                })
 
-            cache.set(cache_key, json_res)
-
+            ComFunc.cache(cache_key, data=json_res)
         return json_res
 
     @classmethod
@@ -52,97 +59,105 @@ class CommonIp(DocType):
         :return:
         """
         ip_str = kwargs.get('ip')
-        _type = kwargs.get('type')
         _interval = kwargs.get('interval', '1h')
 
         cache_key = 'date-record-{0}-{1}'.format(
-            ip_str, _type
+            ip_str, cls._type
         )
-        json_res = cache.get(cache_key)
+        json_res = ComFunc.cache(cache_key)
 
         if not json_res:
             s = cls.search().query("match", ip=ip_str).extra(size=0)
-
-            # agg data
-            s.aggs.bucket('ip_per_hour', 'date_histogram', field='@timestamp',
-                          interval=_interval)
-            s.aggs['ip_per_hour'].bucket('ip_term', 'terms',
-                                         field='ip.keyword')
-            s.aggs['ip_per_hour']['ip_term'].metric('flows_per_hour', 'sum',
-                                                    field='flows')
-            s.aggs['ip_per_hour']['ip_term'].metric('bytes_per_hour', 'sum',
-                                                    field='bytes')
-            s.aggs['ip_per_hour']['ip_term'].metric('packets_per_hour', 'sum',
-                                                    field='packets')
+            s.aggs.bucket(
+                'ip_per_hour', 'date_histogram', field='@timestamp',
+                interval=_interval, time_zone=cls.time_zone
+            )
+            s.aggs['ip_per_hour'].bucket(
+                'ip_term', 'terms', field='ip.keyword'
+            )
+            s.aggs['ip_per_hour']['ip_term'].metric(
+                'flows_per_hour', 'avg', field='flows'
+            )
+            s.aggs['ip_per_hour']['ip_term'].metric(
+                'bytes_per_hour', 'avg', field='bytes'
+            )
+            s.aggs['ip_per_hour']['ip_term'].metric(
+                'packets_per_hour', 'avg', field='packets'
+            )
 
             # cls.debug_query(s)
             response = s.execute()
 
-            json_res = {'datetime': [], 'flows': [],
-                        'bytes': [], 'packets': []}
+            json_res = []
             for dt in response.aggregations.ip_per_hour.buckets:
                 datetime = dt.key_as_string
                 for stats in dt.ip_term.buckets:
-                    json_res['datetime'].append(datetime)
-                    json_res['flows'].append(stats.flows_per_hour.value)
-                    json_res['bytes'].append(
-                        cls.bytes_convert(stats.bytes_per_hour.value, 'mb')
-                    )
-                    json_res['packets'].append(
-                        cls.number_convert(stats.packets_per_hour.value, 'k')
-                    )
-            cache.set(cache_key, json_res)
+                    json_res.append({
+                        'datetime': datetime,
+                        'flows': ComFunc.number_convert(
+                            stats.flows_per_hour.value, 'k'
+                        ),
+                        'packets': ComFunc.number_convert(
+                            stats.packets_per_hour.value, 'k'
+                        ),
+                        'bytes': ComFunc.bytes_convert(
+                            stats.bytes_per_hour.value, 'mb'
+                        )
+                    })
+
+            ComFunc.cache(cache_key, data=json_res)
         return json_res
 
     @classmethod
     def get_all_date_record(cls, **kwargs):
         """
-        get all ip adress date record
+        读取前7个IP的平均 数据流
         :param kwargs:
         :return:
         """
-        _type = kwargs.get('type')
+        # 默认获取每1小时的数据
         _interval = kwargs.get('interval', '1h')
 
-        cache_key = 'all-ip-date-record-{0}'.format(_type)
-        json_res = cache.get(cache_key)
+        cache_key = 'all-ip-date-record-{0}'.format(cls._type)
+        json_res = ComFunc.cache(cache_key)
 
         if not json_res:
             s = cls.search().extra(size=0)
-            # agg data, 1:ips, 2:group by date, 3:ip-avg flows
-            s.aggs.bucket('ips', 'terms', field='ip.keyword', size=7)
-            s.aggs['ips'].bucket('date_avg_flow', 'date_histogram',
-                                 field='@timestamp',
-                                 interval=_interval)
-            s.aggs['ips']['date_avg_flow'].metric('ip_avg_flow', 'avg',
-                                                  field='flows')
+            s.aggs.bucket(
+                'ips', 'terms', field='ip.keyword', size=7,
+                order={"avg_flow": "desc"}
+            )
+            s.aggs['ips'].metric('avg_flow', 'avg', field='flows')
+            s.aggs['ips'].bucket(
+                'date_avg_flow', 'date_histogram', field='@timestamp',
+                time_zone=cls.time_zone, interval=_interval
+            )
+            s.aggs['ips']['date_avg_flow'].metric(
+                'ip_avg_flow', 'avg', field='flows'
+            )
 
             # cls.debug_query(s)
             response = s.execute()
 
-            json_res = {'datetime': []}
+            json_res = {}
             for dt in response.aggregations.ips.buckets:
                 _ip = dt.key
-                json_res[_ip] = {'avg_flow': []}
-                datetime_len = len(dt.date_avg_flow.buckets)
+                json_res[_ip] = []
                 for date_flow in dt.date_avg_flow.buckets:
-                    if datetime_len != len(json_res['datetime']):
-                        json_res['datetime'].append(
-                            date_flow.key_as_string
-                        )
-                    if date_flow.ip_avg_flow.value:
-                        _avg_flow = round(date_flow.ip_avg_flow.value, 2)
-                    else:
-                        _avg_flow = 0
-                    json_res[_ip]['avg_flow'].append(_avg_flow)
+                    json_res[_ip].append({
+                        'avg_flow': ComFunc.number_convert(
+                            date_flow.ip_avg_flow.value
+                        ),
+                        'datetime': date_flow.key_as_string
+                    })
 
-            cache.set(cache_key, json_res)
+            ComFunc.cache(cache_key, data=json_res)
         return json_res
 
 
-class SrcIp(CommonDoc, CommonIp):
+class SrcIp(CommonIp):
     """
-    src ip doc class
+    src ip
     """
     flows = Long()
     bytes = Long()
@@ -153,9 +168,9 @@ class SrcIp(CommonDoc, CommonIp):
         index = 'src-ip-stats-2017.08.02'
 
 
-class DstIp(CommonDoc, CommonIp):
+class DstIp(CommonIp):
     """
-    dst ip doc class
+    dst ip
     """
     flows = Long()
     bytes = Long()
@@ -166,9 +181,9 @@ class DstIp(CommonDoc, CommonIp):
         index = 'dst-ip-stats-2017.08.02'
 
 
-class Netflow(CommonDoc, CommonIp):
+class Netflow(CommonIp):
     """
-    netflow data doc class
+    netflow raw data
     """
 
     class Meta:
@@ -182,23 +197,27 @@ class Netflow(CommonDoc, CommonIp):
         :return:
         """
         _ip = kwargs.get('ip')
+
         cache_key = 'netflow-src-ip-stats-{0}'.format(_ip)
-        json_res = cache.get(cache_key)
+        json_res = ComFunc.cache(cache_key)
 
         if not json_res:
-            s = cls.search().query("match", **{'netflow.ipv4_src_addr': _ip})\
-                .extra(size=0)
-            s.aggs.bucket('dst_ips', 'terms',
-                          field='netflow.ipv4_dst_addr.keyword', size=7)
-            s.aggs['dst_ips'].metric('avg_packet', 'avg',
-                                     field='netflow.in_pkts')
+            s = cls.search().query(
+                "match", **{'netflow.ipv4_src_addr': _ip}
+            ).extra(size=0)
+            s.aggs.bucket(
+                'dst_ips', 'terms', field='netflow.ipv4_dst_addr.keyword'
+            )
+            s.aggs['dst_ips'].metric(
+                'avg_packet', 'avg', field='netflow.in_pkts'
+            )
 
-            cls.debug_query(s)
+            # cls.debug_query(s)
             response = s.execute()
 
             json_res = {}
             for dt in response.aggregations.dst_ips.buckets:
                 json_res[dt.key] = round(dt.avg_packet.value, 2)
 
-            cache.set(cache_key, json_res)
+            ComFunc.cache(cache_key, data=json_res)
         return json_res
