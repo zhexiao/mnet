fdcff0dafccc86a4d31936b4cf7a329ec8b18bcb
zhexiao <zhexiao@163.com>
2019-09-11 10:17:31 +0800

    add kafka

---
+++ deploy/test/kf_test.py
@@ -0,0 +1,13 @@
+from kafka import KafkaProducer
+import time
+
+producer = KafkaProducer(bootstrap_servers='192.168.33.50:9092')
+
+topic = 'test'
+i = 0
+while True:
+    i += 1
+    msg = "my kafka {}".format(i)
+    producer.send(topic, msg.encode())
+    print('producer - {0}'.format(msg))
+    time.sleep(2)
