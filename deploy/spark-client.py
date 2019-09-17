fbf4de823b30a193a1fab5c2739b30ef2e4c844a
zhexiao <zhexiao@163.com>
2019-09-17 11:43:06 +0800

    架构优化

--- deploy/spark-client.py
+++ deploy/spark-client.py
@@ -1,184 +1,83 @@
-# -*- coding: utf-8 -*-
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, window
+from pyspark.sql.types import StructType, IntegerType, StringType, TimestampType
 
-import json
-from pprint import pprint
-from socket import socket, AF_INET, SOCK_DGRAM, error as skerror
+spark_master = "spark://spark-master:7077"
+kafka_master = "kf1:9092,kf2:9092"
+mnet_topic = "mnet"
+mnet_agg_topic = "mnet_agg"
+window_time = "30 seconds"
 
+spark = SparkSession.builder.master(
+    spark_master
+).getOrCreate()
 
-class SparkStreaming:
-    # 定义时间duration
-    batch_duration = 2
-    window_duration = 15*2
-    slide_duration = 5*2
+stream_data = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_master) \
+    .option("subscribe", mnet_topic) \
+    .load()
+stream_data.printSchema()
 
-    # 定义初始化需要的参数
-    spark_master = "spark://spark-master:7077"
-    zookeeper_host = 'zoo1:2181'
-    group_id = 'spark-consumer'
-    topics = {
-        'mnet': 1
-    }
-
-    # 网络数据参数
-    protocol_num = [6, 17]
-    udp_buffer_size = 1024
-
-    logstash_server_host = 'lg1'
-    logstash_server_src_ip_port = 21561
-    logstash_server_dst_ip_port = 21562
-    logstash_server_src_ip_addr = (
-        logstash_server_host, logstash_server_src_ip_port
+# kafka json数据解析
+data_schema = StructType().add(
+    "host", StringType()
+).add(
+    "create_time", TimestampType()
+).add(
+    "netflow", StructType().add(
+        "ipv4_src_addr", StringType()
+    ).add(
+        "ipv4_dst_addr", StringType()
+    ).add(
+        "in_bytes", IntegerType()
+    ).add(
+        "in_pkts", IntegerType()
+    ).add(
+        "protocol", IntegerType()
     )
-    logstash_server_dst_ip_addr = (
-        logstash_server_host, logstash_server_dst_ip_port
-    )
+)
+new_stream_data = stream_data.select(
+    stream_data.key.cast("string"),
+    from_json(stream_data.value.cast("string"), data_schema).alias('json_data')
+)
+new_stream_data.printSchema()
 
-    def __init__(self):
-        # 创建streaming context
-        sc = SparkContext(master=self.spark_master, appName="spark_streaming")
-        ssc = StreamingContext(sc, self.batch_duration)
+new_df = new_stream_data.filter(
+    new_stream_data.json_data.netflow.protocol == 6 or new_stream_data.json_data.netflow.protocol == 17
+).select(
+    (new_stream_data.json_data.netflow.ipv4_src_addr).alias('src_ip'),
+    (new_stream_data.json_data.netflow.in_bytes).alias('in_bytes'),
+    (new_stream_data.json_data.netflow.in_pkts).alias('in_pkts'),
+    (new_stream_data.json_data.create_time).alias('create_time'),
+)
+new_df.printSchema()
 
-        # 链接到kafka zookeeper
-        dstream = KafkaUtils.createStream(
-            ssc, self.zookeeper_host, self.group_id, self.topics)
-        netflow_dstream = dstream.map(lambda ds: json.loads(ds[1]))
+# 聚合
+net_df = new_df.withWatermark(
+    'create_time', window_time
+).groupBy(
+    new_df.src_ip,
+    window(new_df.create_time, window_time, window_time),
+).sum('in_bytes', 'in_pkts')
 
-        # 打印数据
-        # netflow_dstream.pprint()
+res_df = net_df.withColumnRenamed(
+    "sum(in_bytes)","in_bytes"
+).withColumnRenamed(
+    "sum(in_pkts)","in_pkts"
+)
+res_df.printSchema()
 
-        # 统计数据
-        src_ip_stats, dst_ip_stats = self.count_protocol(netflow_dstream)
-
-        # 对统计后的数据进行后续操作
-        src_ip_stats.foreachRDD(
-            lambda rdd: self.process_src_ip_result(rdd.collectAsMap())
-        )
-        dst_ip_stats.foreachRDD(
-            lambda rdd: self.process_dst_ip_result(rdd.collectAsMap())
-        )
-
-        # 启动服务
-        ssc.start()
-        ssc.awaitTermination()
-
-    def count_protocol(self, netflow_dstream):
-        """
-        统计IP地址数据
-        :param netflow_dstream:
-        :return:
-        """
-        # 过滤，只返回tcp和udp的DStream数据
-        p_flow = netflow_dstream.filter(
-            lambda n_dt: (n_dt['netflow']['protocol'] in self.protocol_num)
-        )
-
-        # 重新map一个DStream，以源IP为key组装起来
-        src_ip_mapped = p_flow.map(lambda dt: (
-            (dt['netflow']['ipv4_src_addr']),
-            (1, dt['netflow']['in_pkts'], dt['netflow']['in_bytes'])
-        ))
-        dst_ip_mapped = p_flow.map(lambda dt: (
-            (dt['netflow']['ipv4_dst_addr']),
-            (1, dt['netflow']['in_pkts'], dt['netflow']['in_bytes'])
-        ))
-
-        # 先对数据做个小的统计
-        src_ip_reduced = src_ip_mapped.reduceByKey(lambda v1, v2: (
-            v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2]
-        ))
-        dst_ip_reduced = dst_ip_mapped.reduceByKey(lambda v1, v2: (
-            v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2]
-        ))
-
-        # 然后对每30秒数据每10秒钟统计一次
-        src_ip_stats = src_ip_reduced.window(
-            self.window_duration, self.slide_duration
-        ).reduceByKey(
-            lambda v1, v2: (v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2])
-        )
-        dst_ip_stats = dst_ip_reduced.window(
-            self.window_duration, self.slide_duration
-        ).reduceByKey(
-            lambda v1, v2: (v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2])
-        )
-
-        return src_ip_stats, dst_ip_stats
-
-    def process_src_ip_result(self, result):
-        """
-        处理统计得到的src ip数据
-        :return:
-        """
-        for ip, val in result.items():
-            json_data = {
-                'ip': ip,
-                'flows': val[0],
-                'packets': val[1],
-                'bytes': val[2]
-            }
-            pprint('src ip - '*50)
-            pprint('+' * 50)
-            pprint('+' * 50)
-            pprint(json.dumps(json_data))
-            pprint('+' * 50)
-            pprint('+' * 50)
-            pprint('src ip - ' * 50)
-
-            self.send_src_ip_data(json.dumps(json_data))
-
-    def process_dst_ip_result(self, result):
-        """
-        处理统计得到的dst ip数据
-        :return:
-        """
-        for ip, val in result.items():
-            json_data = {
-                'ip': ip,
-                'flows': val[0],
-                'packets': val[1],
-                'bytes': val[2]
-            }
-            pprint('dst ip - '*50)
-            pprint('+' * 50)
-            pprint('+' * 50)
-            pprint(json.dumps(json_data))
-            pprint('+' * 50)
-            pprint('+' * 50)
-            pprint('dst ip -' * 50)
-
-            self.send_dst_ip_data(json.dumps(json_data))
-
-    def send_src_ip_data(self, json_string):
-        """
-        使用网络传输src ip数据到input server的logstash
-        :param json_string:
-        :return:
-        """
-        udp_socket = socket(AF_INET, SOCK_DGRAM)
-        try:
-            udp_socket.sendto(json_string.encode(), self.logstash_server_src_ip_addr)
-        except skerror:
-            pprint("cannot connect to udp server")
-        finally:
-            udp_socket.close()
-
-    def send_dst_ip_data(self, json_string):
-        """
-        使用网络传输dst ip数据到input server的logstash
-        :param json_string:
-        :return:
-        """
-        udp_socket = socket(AF_INET, SOCK_DGRAM)
-        try:
-            udp_socket.sendto(json_string.encode(), self.logstash_server_dst_ip_addr)
-        except skerror:
-            pprint("cannot connect to udp server")
-        finally:
-            udp_socket.close()
-
-
-# 运行类
-SparkStreaming()
+# Start running the query that prints the running counts to the console
+query = res_df \
+    .selectExpr("CAST(window AS STRING) AS key", "to_json(struct(*)) AS value") \
+    .writeStream \
+    .trigger(processingTime=window_time) \
+    .outputMode("update") \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_master) \
+    .option("topic", mnet_agg_topic) \
+    .option("checkpointLocation", "/tmp/{}".format(mnet_agg_topic)) \
+    .start() \
+    .awaitTermination()
